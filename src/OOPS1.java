47f53e3e37fbb91843522aa0cc37e7adbdf53a3d
Mridul Birla <mridulbirla13@gmail.com>
2023-03-28 15:10:47 +0530

    Parameterized Constructor used

--- src/OOPS1.java
+++ src/OOPS1.java
@@ -1,7 +1,21 @@
 class Student{
     String name;
     int age;
+
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+
+    Student(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
 }
 
 public class OOPS1 {
+    public static void main(String[] args){
+        Student s1 = new Student("Mridul",21); //Student() is Non- Parametrised Constructor
+        s1.printInfo();
+    }
 }
